ba78087a93c34cfc483c330c376975ae4e34408b
[dyad] <gustavogama099@gmail.com>
2025-09-13 16:05:24 -0300

    [dyad] Atualizando importações de React para 'import * as React from 'react';' - wrote 3 file(s)

--- src/pages/AgencyPlaybookPage.tsx
+++ src/pages/AgencyPlaybookPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
 import { showError, showSuccess } from "@/utils/toast";
